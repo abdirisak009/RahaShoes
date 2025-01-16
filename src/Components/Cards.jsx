2b5a05fdcdaa80fc476fac729963531180686eac
fartuun <fartun@gmail.com>
2025-01-16 07:58:52 +0300

    added cards

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -3,7 +3,7 @@ import { assets } from '../assets/assets'
 
 const Cards = () => {
   return (
-    <div className='container px-8 py-5'>
+    <div className='container px-8 py-5 bg-[#afdff6] mt-5'>
         <div className='grid grid-cols-1 md:grid-cols-4 gap-3'>
             <div className="flex shadow-lg px-5 py-8 border-2 border-grey-500 ">
                 <img className='h-14 px-2' src= {assets.dileveryn_icon} alt="" />
@@ -13,24 +13,24 @@ const Cards = () => {
                </div>
             </div>
             <div className="flex shadow-lg px-5 py-8 border-2 border-grey-500 ">
-                <img className='h-14 px-2' src= {assets.dileveryn_icon} alt="" />
+                <img className='h-14 px-2' src= {assets.support_icon} alt="" />
                <div>
-               <h2 className='font-bold text-2xl'>Free Dilevery</h2>
-               <p>Free Shipping for orders over $20</p>
+               <h2 className='font-bold text-2xl'>Support 24/7</h2>
+               <p>24 hours a day, 7 days a week</p>
                </div>
             </div>
             <div className="flex shadow-lg px-5 py-8 border-2 border-grey-500 ">
-                <img className='h-14 px-2' src= {assets.dileveryn_icon} alt="" />
+                <img className='h-14 px-2' src= {assets.payment_icon} alt="" />
                <div>
-               <h2 className='font-bold text-2xl'>Free Dilevery</h2>
-               <p>Free Shipping for orders over $20</p>
+               <h2 className='font-bold text-2xl'>Payment</h2>
+               <p>Pay with Waafi Visa Card</p>
                </div>
             </div>
             <div className="flex shadow-lg px-5 py-8 border-2 border-grey-500 ">
-                <img className='h-14 px-2' src= {assets.dileveryn_icon} alt="" />
+                <img className='h-14 px-2' src= {assets.grant_icon} alt="" />
                <div>
-               <h2 className='font-bold text-2xl'>Free Dilevery</h2>
-               <p>Free Shipping for orders over $20</p>
+               <h2 className='font-bold text-2xl'>Guarantee</h2>
+               <p>Within 30 days for an exchange</p>
                </div>
             </div>
             
--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,6 +2,9 @@ import logo from './logo.png'
 import cross_icon from './cross_icon.svg'
 import menu_icon from './menu_icon.svg'
 import dileveryn_icon from './dilevery_icon.png'
+import payment_icon from './payment_icon.png'
+import support_icon from './support_icon.png'
+import grant_icon from './grant_icon.png'
 
 
 
@@ -9,7 +12,10 @@ export const assets = {
     logo,
     cross_icon,
     menu_icon,
-    dileveryn_icon
+    dileveryn_icon,
+    payment_icon,
+    support_icon,
+    grant_icon
    
    
 }
